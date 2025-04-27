1c44e71025e9b8b3b32a9cd1cc53bb28c719b3f0
Yogurtheyork <york4818@gmail.com>
2025-04-28 00:43:15 +0800

    Change main
    Change main to call ActiveUI.

--- src/main/java/Active/Main.java
+++ src/main/java/Active/Main.java
@@ -1,44 +1,9 @@
-package UI;
+package Active;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import UI.*;
 
 public class Main {
-
-    public static final JFrame mainFrame = new JFrame();
-    public static final JPanel calenderPanel = new JPanel(new BorderLayout());
-    public static final JPanel chatRoomPanel = new JPanel(new BorderLayout());
-
     public static void main(String[] args) {
-
-        // ChatRoom panel initialization
-        ChatRoom chatRoom = new ChatRoom();
-        JButton chatRoomButton = new JButton("Toggle Chat");
-        chatRoomButton.setPreferredSize(new Dimension(100, 30));
-        chatRoomPanel.add(chatRoomButton, BorderLayout.SOUTH);
-        chatRoomPanel.add(chatRoom, BorderLayout.CENTER);
-        chatRoomPanel.setPreferredSize(new Dimension(300, 600));
-        chatRoomPanel.setVisible(true);
-        chatRoomButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                chatRoom.setVisible(!chatRoom.isVisible());
-            }
-        });
-
-        CalendarUI calendarUI = new CalendarUI();
-        mainFrame.add(calendarUI, BorderLayout.CENTER);
-
-
-        // MainFrame initialization
-        mainFrame.setSize(1200, 800);
-        mainFrame.setMinimumSize(new Dimension(800, 600));
-        //mainFrame.add(calenderPanel, BorderLayout.CENTER);
-        mainFrame.add(calenderPanel, BorderLayout.CENTER);
-        mainFrame.add(chatRoomPanel, BorderLayout.EAST);
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setVisible(true);
+        ActiveUI ui = new ActiveUI();
     }
 }
---
+++ src/main/java/UI/ActiveUI.java
@@ -0,0 +1,43 @@
+package UI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ActiveUI {
+    public static final JFrame mainFrame = new JFrame();
+    public static final JPanel calenderPanel = new JPanel(new BorderLayout());
+    public static final JPanel chatRoomPanel = new JPanel(new BorderLayout());
+
+    public ActiveUI() {
+
+        // ChatRoom panel initialization
+        ChatRoom chatRoom = new ChatRoom();
+        JButton chatRoomButton = new JButton("Toggle Chat");
+        chatRoomButton.setPreferredSize(new Dimension(100, 30));
+        chatRoomPanel.add(chatRoomButton, BorderLayout.SOUTH);
+        chatRoomPanel.add(chatRoom, BorderLayout.CENTER);
+        chatRoomPanel.setPreferredSize(new Dimension(300, 600));
+        chatRoomPanel.setVisible(true);
+        chatRoomButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                chatRoom.setVisible(!chatRoom.isVisible());
+            }
+        });
+
+        CalendarUI calendarUI = new CalendarUI();
+        mainFrame.add(calendarUI, BorderLayout.CENTER);
+
+
+        // MainFrame initialization
+        mainFrame.setSize(1200, 800);
+        mainFrame.setMinimumSize(new Dimension(800, 600));
+        //mainFrame.add(calenderPanel, BorderLayout.CENTER);
+        mainFrame.add(calenderPanel, BorderLayout.CENTER);
+        mainFrame.add(chatRoomPanel, BorderLayout.EAST);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
+    }
+}
