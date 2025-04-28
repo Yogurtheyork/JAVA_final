294bccaedf23ca740bd99e98cb2a9f93ddd29c97
Yanagi0912 <930912jeffery@gmail.com>
2025-04-28 21:13:31 +0800

    AI處理事件檔案

--- src/main/java/ChatGPT/Prompt/EventPrompt.java
+++ src/main/java/ChatGPT/Prompt/EventPrompt.java
@@ -1,4 +1,14 @@
 package ChatGPT.Prompt;
 
 public class EventPrompt {
+    // TODO: 將語言轉換寫進json檔
+    private static final String ChineseStart =
+            "請作為一個[學習助理]協助我安排行程，並提供我[行程名稱]、[開始時間]、[結束時間]、[地點]、[備註]等資訊。" +
+                    "以下json格式資料是[現有的行程]，";
+    private static final String ChineseLearning = "";
+    private static final String ChineseReview = "";
+    private static final String ChineseEnd = "";
+    private static final String EnglishStart =
+            "Please act as a [Learning Assistant] to help me schedule, and provide me with information such as [Event Name], [Start Time], [End Time], [Location], and [Notes]." +
+                    "The following json format data is [existing schedule],";
 }
