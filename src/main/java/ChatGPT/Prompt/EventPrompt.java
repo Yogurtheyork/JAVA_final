103fa1918b300b2935228c890312fb4dc82cf2cf
Yanagi0912 <930912jeffery@gmail.com>
2025-04-28 21:25:07 +0800

    Review方法(Prompt未完成)

--- src/main/java/ChatGPT/Prompt/EventPrompt.java
+++ src/main/java/ChatGPT/Prompt/EventPrompt.java
@@ -1,5 +1,7 @@
 package ChatGPT.Prompt;
 
+import ChatGPT.ChatConfig;
+
 public class EventPrompt {
     // TODO: 將語言轉換寫進json檔
     private static final String ChineseStart =
@@ -11,4 +13,25 @@ public class EventPrompt {
     private static final String EnglishStart =
             "Please act as a [Learning Assistant] to help me schedule, and provide me with information such as [Event Name], [Start Time], [End Time], [Location], and [Notes]." +
                     "The following json format data is [existing schedule],";
+    private static final String EnglishLearning = "";
+    private static final String EnglishReview = "";
+    private static final String EnglishEnd = "";
+
+    public String ReviewPrompt (){
+        String prompt;
+        if (ChatConfig.loadLanguage().equals("zh")) {
+            prompt = ChineseStart+
+                    ChineseReview+
+                    ChineseEnd;
+        } else if(ChatConfig.loadLanguage().equals("en")) {
+            prompt = EnglishStart+
+                    EnglishReview+
+                    EnglishEnd;
+        } else {
+            prompt = EnglishStart+
+                    EnglishReview+
+                    EnglishEnd;
+        }
+        return prompt+"\n";
+    }
 }
